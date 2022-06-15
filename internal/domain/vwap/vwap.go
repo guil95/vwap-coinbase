f5d90347ceac1fb57e4426996689e5b9bd16baf0
Guilherme Rodrigues <guilhermerodriguestb@gmail.com>
2022-06-15 12:19:43 -0300

    Resolve fix sprintf

--- internal/domain/vwap/vwap.go
+++ internal/domain/vwap/vwap.go
@@ -14,5 +14,5 @@ func NewVwap() Vwap {
 }
 
 func (v *Vwap) Calc(tradeName string, traders []trader.TradeResponse) {
-	fmt.Println(fmt.Sprintf("Trade Name: %s trade len %v first %v last %v", tradeName, len(traders)), traders[0], traders[len(traders)-1])
+	fmt.Println(fmt.Sprintf("Trade Name: %s trade len %v first %v last %v", tradeName, len(traders), traders[0], traders[len(traders)-1]))
 }
