876b54c13dd2a0dbd6d73e8ea28033014a8df7fa
Guilherme Rodrigues <guilhermerodriguestb@gmail.com>
2022-06-16 21:27:15 -0300

    Added unit tests

--- internal/domain/vwap/vwap.go
+++ internal/domain/vwap/vwap.go
@@ -3,6 +3,7 @@ package vwap
 import (
 	"github.com/guil95/vwap-coinbase/internal/domain/trader"
 	"github.com/shopspring/decimal"
+	"log"
 )
 
 type Vwap struct {
@@ -12,18 +13,26 @@ func NewVwap() Vwap {
 	return Vwap{}
 }
 
+const minTradersToCalc = 1
+
 func (v *Vwap) Calc(traders []trader.TradeResponse) (decimal.Decimal, error) {
 	sumVolPrice := decimal.NewFromInt(0)
 	sumVol := decimal.NewFromInt(0)
 
+	if len(traders) <= minTradersToCalc {
+		return decimal.Decimal{}, VwapInvalidQtdTraderError{}
+	}
+
 	for _, trade := range traders {
 		tradeVol, err := decimal.NewFromString(trade.Size)
 		if err != nil {
-			return decimal.Decimal{}, err
+			log.Print(err)
+			return decimal.Decimal{}, VwapInvalidSizeError{}
 		}
 		tradePrice, err := decimal.NewFromString(trade.Price)
 		if err != nil {
-			return decimal.Decimal{}, err
+			log.Print(err)
+			return decimal.Decimal{}, VwapInvalidPriceError{}
 		}
 		sumVol = sumVol.Add(tradeVol)
 		sumVolPrice = sumVolPrice.Add(tradePrice.Mul(tradeVol))
